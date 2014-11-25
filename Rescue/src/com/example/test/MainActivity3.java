4e5fb7452d2d1048ee942d6740b78a4f9f1b1c75
KeyboardWarriors <bramach6@in.ibm.com>
2014-11-25 18:30:29 +0530

    You can be someone's rescue story
    The  main motive behind the creation of this application is to save
    lives. The application gets information from the Blood-Banks and from
    the Organ donor Organizations . The doctors from the hospitals can use
    the application to check the inventory of the Blood-Banks for the
    availability of blood groups in the vicinity, The same also applies for
    the availability of organs or finding a match for a bone marrow
    transplant. The application also allows people to register with the
    blood group details. The application will alert the volunteer incase of
    an emergency , where the hospital is unable to find a match in the
    nearby Blood-Banks. The application will also alert the volunteers if a
    blood-donation camp or a registration for organ donation is occurring
    nearby.

---
+++ Rescue/src/com/example/test/MainActivity3.java
@@ -0,0 +1,42 @@
+package com.example.test;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity3 extends Activity {
+
+   @SuppressLint("NewApi")
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main_activity3);
+      getActionBar().setDisplayHomeAsUpEnabled(true);
+   }
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.main3, menu);
+      return true;
+   }  
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+      switch (item.getItemId()) {
+         // Respond to the action bar's Up/Home button
+         case android.R.id.home:
+         NavUtils.navigateUpFromSameTask(this);
+         return true;
+      }
+      return super.onOptionsItemSelected(item);
+   }
+   @Override
+   public void onBackPressed() {
+      moveTaskToBack(true); 
+      MainActivity3.this.finish();
+   }
+}
